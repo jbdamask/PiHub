5f49db8a18366ae35207e474c65a0f6f1731a066
damasjo1 <john.damask@novartis.com>
2017-08-04 13:25:01 -0400

    renamed

---
+++ BluefruitManager.py
@@ -0,0 +1,40 @@
+from bluepy.btle import Scanner, DefaultDelegate, Peripheral
+import threading
+
+
+class DeviceScanner(threading.Thread):
+
+    # Limit the number of ble devices per Pi
+    _deviceLimit = 8
+    _registeredDevices = {}
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.scanner = Scanner(0)
+        self.daemon = True
+
+    def run(self):
+        print "Starting scanner thread"
+        while True:
+            _devices = self.scanner.scan(10.0)
+            _onlineDeviceAddresses = {}
+            _counter = 0
+            for d in _devices:
+                # If we're over the limit just stop
+                while _counter < self._deviceLimit:
+                    _onlineDeviceAddresses[d.addr] = ""
+                    for (adtype, desc, value) in d.getScanData():
+                        if value == "Adafruit Bluefruit LE":
+                            # Check only to avoid unnecessary locking
+                            if d.addr not in self._registeredDevices:
+                                _onlineDeviceAddresses[d.addr] = ""
+                                _counter += 1
+
+            # Replace registered devices with ones that are currently online
+            self._registeredDevices = _onlineDeviceAddresses
+
+    def getDeviceLimit(self):
+        return self._deviceLimit
+
+    def getDevices(self):
+        return self._registeredDevices
