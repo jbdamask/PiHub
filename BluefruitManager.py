7c30c587497774bae43fab575b492677064c5b74
damasjo1 <john.damask@novartis.com>
2017-08-04 14:08:20 -0400

    better logic around add/removal of registered devices

--- BluefruitManager.py
+++ BluefruitManager.py
@@ -20,23 +20,32 @@ class DeviceScanner(threading.Thread):
             print "Scanning..."
             _devices = self.scanner.scan(10.0)
             _onlineDeviceAddresses = {}
-            _counter = 0
+            #_counter = 0
             print "Iterating over devices..."
             for d in _devices:
                 # If we're over the limit just stop
-                if _counter < self._deviceLimit:
-                    print _counter
-                    for (adtype, desc, value) in d.getScanData():
-                        if value == "Adafruit Bluefruit LE":
-                            print "Found Bluefruit with address: " + d.addr
+                #if _counter < self._deviceLimit:
+                #    print _counter
+                for (adtype, desc, value) in d.getScanData():
+                    if value == "Adafruit Bluefruit LE":
+                        print "Found Bluefruit with address: " + d.addr
+                        _onlineDeviceAddresses[d.addr] = ""
                             # Check only to avoid unnecessary locking
-                            if d.addr not in self._registeredDevices:
-                                print d.addr + " is online!"
-                                _onlineDeviceAddresses[d.addr] = ""
-                                _counter += 1
+                            #if d.addr not in self._registeredDevices:
+                            #    print d.addr + " is online!"
+                                #_onlineDeviceAddresses[d.addr] = ""
+                                #_counter += 1
+                            #_counter += 1
 
             # Replace registered devices with ones that are currently online
-            self._registeredDevices = _onlineDeviceAddresses
+            # Remove devices we can't see
+            for r in self._registeredDevices:
+                if r not in _onlineDeviceAddresses:
+                    del self._registeredDevices[r]
+            # Add devices we can (provided there's still space)
+            for n in _onlineDeviceAddresses:
+                if (n not in self._registeredDevices) and (len(self._registeredDevices) < self._deviceLimit):
+                    self._registeredDevices[n] = ""
         time.sleep(2)
 
     def getDeviceLimit(self):
