50e7ea90d201eeea595fb131ffdc17c27f9e95c7
damasjo1 <john.damask@novartis.com>
2017-08-04 21:28:08 -0400

    blah

--- AWSIoTMQTTShadowClientGenerator.py
+++ AWSIoTMQTTShadowClientGenerator.py
@@ -97,9 +97,11 @@ class AWSIoTMQTTShadowClientGenerator:
         print "updateState() msg:"
         print "  " + json.dumps(value)
         self._reported_state[value["MAC"]] = value
-        if len(self._desired_state) > 0:
-            for x in self._desired_state:
-                self._desired_state[x]["color"] = value["color"]
+        self._desired_state[value["MAC"]] = value
+        if len(self._desired_state) > 1:
+            for x in self._desired_state.keys():
+                if x != value["MAC"]:
+                    self._desired_state[x]["color"] = value["color"]
         else:
             self._desired_state[value["MAC"]] = value
         print "Desired state values: " + json.dumps(self._desired_state.values())
