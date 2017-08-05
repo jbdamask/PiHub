7e71ee09c146bdafbd523b69f84b9d301fb9577d
damasjo1 <john.damask@novartis.com>
2017-08-05 11:48:43 -0400

    silly shit

--- AWSIoTMQTTShadowClientGenerator.py
+++ AWSIoTMQTTShadowClientGenerator.py
@@ -18,7 +18,7 @@ class ShadowCallbackContainer:
         print(deltaMessage)
         print("Request to update the reported state...")
         newPayload = '{"state":{"reported":' + deltaMessage + '}}'
-        print "Calling deviceShadowInstance which is type " + deviceShadowInstance.__class__.__name__
+        print "Calling deviceShadowInstance which is type " + self.deviceShadowInstance.__class__.__name__
         self.deviceShadowInstance.shadowUpdate(newPayload, None, 5)
         print("Sent.")
 
