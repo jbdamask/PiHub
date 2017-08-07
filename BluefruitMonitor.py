51792736b68ce8fe8201f9b8e3dc2c14d7e56667
damasjo1 <john.damask@novartis.com>
2017-08-06 20:17:53 -0400

    strange errors with new ble

--- BluefruitMonitor.py
+++ BluefruitMonitor.py
@@ -72,7 +72,7 @@ class BluefruitMonitor(threading.Thread):
                 return 0
         except:
             e = sys.exc_info()[0]
-            print(str(datetime.now()) + " BluefruitMonitor Error on call to TX: %s" % e)
+            print(str(datetime.now()) + " BluefruitMonitor Error on call to TX: %s" % e.message)
             return 0
 
 
